5c92652ab5b0bf852d7eaac03232200d8d1c59d9
Charles Peterson <CPETER52@depaul.edu>
2021-05-26 13:10:55 -0500

    initial c++ file to hold network class

---
+++ network.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+
+class network
+{
+
+
+    public:
+
+
+	    void testMethod() {
+	    
+		    std::cout << "testing testing 123 " << '\n';
+	        
+	    }
+	
+
+	
+
+
+
+
+
+
+};
+
+
+
+
+
+int main()
+{
+
+network myObj;
+myObj.testMethod();
+
+
+
+return 0;
+}
